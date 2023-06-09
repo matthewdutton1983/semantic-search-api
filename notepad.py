cd7365a68307d6b66bd7388e0184d2d5635170e5
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-06-08 20:13:03 -0400

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -69,271 +69,15 @@ def faiss_index_exists() -> bool:
     return os.path.isfile(f"{USE_CASE}.faiss")
 
 def get_access_token(username: str, password: str) -> str:
-    """Retrieve access token for given user"""
-    url = "https://<IDA>"
-    payload = {
-        "client_id": "<CLIENT_ID>",
-        "grant_type": "password",
-        "username": username,
-        "password": password,
-        "resource": "<RESOURCE>"
-    }
-    response = requests.post(url, payload)
-    token = response.json()["access_token"]    
-    return token
+    """Retrieve doclink token for given user"""
+    pass
 
 def get_document_metadata(unique_id: str, token: str) -> str:
     """Retrieve document metadata"""
-    url = "<URL>"
-    payload = {}
-    headers = {
-        "Accept": "application/json",
-        "Authorization": "Bearer" + token
-    }
-    response = requests.get(url=url, headers=headers, data=payload)
-    return response.json()
+    pass
 
 def get_document_text(unique_id, token) -> str:
-    url = "<URL>"
-    payload = {}
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer" + token
-    }
-    response = requests.get(url=url, headers=headers, data=payload)
-    return response.text
-
-def process_sentences(text: str) -> str:
-    sentences = sent_tokenize(text)
-
-    sentences_with_offsets = []
-    
-    current_position = 0
-    idx = 0
-
-    for sentence in sentences:
-        start = current_position
-        end = start + len(sentence)
-        
-        sentences_with_offsets.append({
-            "idx": idx, 
-            "sentence": sentence, 
-            "start": start, 
-            "end": end
-        })
-        
-        current_position = end + 1
-        idx += 1
-    
-    return sentences_with_offsets
-
-def preprocess_text(text: str) -> str:
-    text = re.sub(r'[""\"]', '', text)
-    text = re.sub(r'\r?\n', ' ', text)
-    text = re.sub(r'\s+', ' ', text).strip()
-    text = text.translate(str.maketrans('', '', string.punctuation))
-    return text.lower()
-
-# Check to see if the database and index exist
-database_exists = db_exists()
-index_exists = faiss_index_exists()
-
-# Connect to existing database or create new one
-if database_exists:
-    engine = create_engine(f"sqlite:///{USE_CASE}.db", echo=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    logging.info("Connected to existing database.")
-else:
-    engine = create_engine(f"sqlite:///{USE_CASE}.db", echo=True)
-    metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-# Load existing index or create new one
-if index_exists:
-    faiss_index = faiss.read_index(f"{USE_CASE}.faiss")
-    logging.info("Loaded Faiss index from disk.")
-else:
-    dim = 512
-    faiss_index = faiss.IndexFlatL2(dim)
-    faiss_index = faiss.IndexIDMap(faiss_index)
-
-# Only fetch document data if the database or index don't exist
-if not database_exists or not index_exists:
-    # Load credentials
-    config = ConfigParser()
-    config.read("H:/jpmDesk/Desktop/credentials.ini")
-    username = config.get("default", "username")
-    password = config.get("default", "password")
-
-    # Load document data
-    data = pd.read_excel(<FILEPATH>, engine="openpyxl")
-    unique_ids = list(data["UnifiedDocID"])
-    
-    # Process documents
-    count = 0
-
-    batch_sentences = []
-    batch_sent_idx = []
-    batch_embeddings = []
-
-    BATCH_SIZE = 10000
-
-    for unique_id in unique_ids:
-        try:
-            if count % BATCH_SIZE == 0:
-                token = get_access_token(username, password)
-                logging.info(f"New token fetched for batch starting at {count}.")
-
-            document_text = get_document_text(unique_id, token)
-            document_metadata = get_document_metadata(unique_id, token)
-            document_name = document_metadata["metadata"]["core"]["documentName"]
-
-            raw_sentences = sent_tokenize(document_text)
-
-            for raw_sentence in raw_sentences:
-                clean_sentence = process_sentences(raw_sentences)
-
-                if not clean_sentence.strip():
-                    continue
-
-                embedding = embed([clean_sentence]).numpy()[0].tolist()
-                batch_embeddings.append(embedding)
-
-                batch_sentences.append({
-                    "sent_idx": count,
-                    "text": raw_sentence,
-                    "document": {"id": unique_id, "name": document_name}
-                })
-
-                count += 1
-
-                if count % BATCH_SIZE == 0:
-                    with session.begin():
-                        session.execute(sentences_table.insert(), batch_sentences)
-                        faiss_index.add_with_ids(np.array(batch_embeddings), np.array(batch_sent_idx))
-
-                        batch_sentences = []
-                        batch_sent_idx = []
-                        batch_embeddings = []
-
-                        logging.info(f"Loaded {count} sentences.")
-        except Exception as e:
-            logging.error(f"Error processing document with id {unique_id}: {str(e)}")
-
-        if batch_sentences:
-            with session.begin():
-                session.execute(sentences_table.insert(), batch_sentences)
-                faiss_index.add_with_ids(np.array(batch_embeddings), np.array(batch_sent_idx))                        
-
-        faiss.write_index(faiss_index, f"{USE_CASE}.faiss")
-        logging.info("Finished processing documents.")
-
-# Import standard libraries
-import logging
-import os
-import re
-import string
-from typing import Any, Dict, List, Union
-
-# Import third-party libraries
-import faiss
-import nltk
-import numpy as np
-import pandas as pd
-import requests
-import tensorflow_hub as hub
-import uvicorn
-from configparser import ConfigParser
-from fastapi import FastAPI
-from nltk.tokenize import sent_tokenize
-from pydantic import BaseModel
-from sqlalchemy import create_engine, inspect, select, Table, Column, Integer, MetaData, PickleType, String
-from sqlalchemy.orm import sessionmaker
-
-# Initialize logging
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-# Define use case
-USE_CASE = "agreementsTest"
-
-# Download NLTK data
-def download_nltk_data():
-    """Download necessary NLTK data"""
-    nltk_data_dir = os.path.expanduser("~/nltk-data")
-    punkt_path = os.path.join(nltk_data_dir, "tokenizers/punkt"):
-
-    if not os.path.exists(punkt_path):
-        nltk.download("punkt", quiet=True)
-
-download_nltk_data()
-
-# Define the database schema
-metadata = MetaData()
-sentences_table = Table(f"{USE_CASE}", metadata, Column("sent_idx", Integer, primary_key=True), 
-                        Column("text", String), Column("document", PickleType))
-
-# Load Universal Sentence Encoder
-embed = hub.load("I:/universal-sentence-encoder")
-
-# Define the search request model
-class Search(BaseModel):
-    query: str
-    num_results: int = 10
-    context: int = 0
-
-# Create the FastAPI app
-app = FastAPI(
-    title="Semantic Similarity Search Demo",
-    description="This is a simple API for conducting semantic similarity searches to find language in Executed Agreements.",
-    version="1.0.0"
-)
-
-def db_exists() -> bool:
-    """Check if the database exists"""
-    engine = create_engine(f"sqlite:///{USE_CASE}.db", echo=True)
-    inspector = inspect(engine)
-    return USE_CASE in inspector.get_table_names()
-
-def faiss_index_exists() -> bool:
-    """Check if the index exists"""
-    return os.path.isfile(f"{USE_CASE}.faiss")
-
-def get_access_token(username: str, password: str) -> str:
-    """Retrieve access token for given user"""
-    url = "https://<IDA>"
-    payload = {
-        "client_id": "<CLIENT_ID>",
-        "grant_type": "password",
-        "username": username,
-        "password": password,
-        "resource": "<RESOURCE>"
-    }
-    response = requests.post(url, payload)
-    token = response.json()["access_token"]    
-    return token
-
-def get_document_metadata(unique_id: str, token: str) -> str:
-    """Retrieve document metadata"""
-    url = "<URL>"
-    payload = {}
-    headers = {
-        "Accept": "application/json",
-        "Authorization": "Bearer" + token
-    }
-    response = requests.get(url=url, headers=headers, data=payload)
-    return response.json()
-
-def get_document_text(unique_id, token) -> str:
-    url = "<URL>"
-    payload = {}
-    headers = {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer" + token
-    }
-    response = requests.get(url=url, headers=headers, data=payload)
-    return response.text
+    pass
 
 def process_sentences(text: str) -> str:
     sentences = sent_tokenize(text)
@@ -479,16 +223,25 @@ def semantic_search(query: str, num_results: int = 10, context: int = 0) -> List
         result = session.execute(stmt).fetchone()
 
         if result is not None:
-            documents = result[3]
+            document = result[3]
             original_text = result[1]
+            document_id = document["id"]
 
             if context > 0:
                 # Fetch sentences before matched sentence to provide context
-                stmt_before = select(sentences_table).where(sentences_table.c.sent_idx >= index_id - context, sentences_table.c.sent_idx < index_id)
+                stmt_before = select(sentences_table).where(
+                    sentences_table.c.sent_idx >= index_id - context,
+                    sentences_table.c.sent_idx < index_id,
+                    sentences_table.c.document["id"] == document_id
+                )
                 result_before = session.execute(stmt_before).fetchall()
 
                 # Fetch sentences after matched sentence to provide context
-                stmt_after = select(sentences_table).where(sentences_table.c.sent_idx > index_id, sentences_table.c.sent_idx <= index_id + context)                
+                stmt_after = select(sentences_table).where(
+                    sentences_table.c.sent_idx > index_id,
+                    sentences_table.c.sent_idx <= index_id + context,
+                    sentences_table.c.document["id"] == document_id
+                )                
                 result_after = session.execute(stmt_after).fetchall()
 
                 context_sentences = [r[1] for r in result_before] + [original_text] + [r[1] for r in result_after]
@@ -498,7 +251,7 @@ def semantic_search(query: str, num_results: int = 10, context: int = 0) -> List
 
             sentence_info = {
                 "text": context_sentences,
-                "documents": documents,
+                "document": document,
                 "similarity_score": float(similarity_score)
             }
             results.append(sentence_info)
@@ -506,26 +259,6 @@ def semantic_search(query: str, num_results: int = 10, context: int = 0) -> List
             logging.info(f"No result found for index_id {index_id}")
 
     return results
-
-@app.get("/health")
-def health_check() -> Dict[str, Any]:
-    """Returns a message indicating that the server is running"""
-    return {"message": "The server is running."}
-
-@app.post("/search")
-def search(request: Search) -> Dict[str, Any]:
-    """Perform a semantic search and return the results"""
-    query = request.query
-    num_results = request.num_results
-    context = request.context
-    results = semantic_search(query, num_results, context) 
-    return {"results": results}
-
-if __name__ == "__main__":
-    try:
-        uvicorn.run(app, host="0.0.0.0", port=8000)
-    finally:
-        session.close()
 
 @app.get("/health")
 def health_check() -> Dict[str, Any]:
